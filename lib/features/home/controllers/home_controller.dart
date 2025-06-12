0c5e83267cc8cbc6c957546208070ae973df4187
Tosin Babatubnde <43211627+tosyno87@users.noreply.github.com>
2025-06-12 15:14:59 -0400

    refactor home page into widgets

---
+++ lib/features/home/controllers/home_controller.dart
@@ -0,0 +1,68 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:provider/provider.dart';
+import 'package:naijasingles/features/ads/google_ads.dart';
+import 'package:naijasingles/features/ads/load_ads.dart';
+
+import '../../../common/data/repo/user_search_repo.dart';
+import '../../../common/providers/user_provider.dart';
+import '../../../models/user_model.dart';
+import '../../../common/constants/constants.dart';
+
+class AdsManager {
+  InterstitialAd? interstitialAd;
+  bool isInterstitialAdReady = false;
+
+  void load() {
+    InterstitialAd.load(
+      adUnitId: AdHelper.interstitialAdUnitId,
+      request: const AdRequest(),
+      adLoadCallback: InterstitialAdLoadCallback(
+        onAdLoaded: (InterstitialAd ad) {
+          interstitialAd = ad;
+        },
+        onAdFailedToLoad: (LoadAdError error) {
+          log('InterstitialAd failed to load: $error');
+        },
+      ),
+    );
+  }
+
+  void showIfNeeded(int count) {
+    if (count % 5 == 0) {
+      LoadAds.loadInterstitialAd(interstitialAd, isInterstitialAdReady);
+      interstitialAd?.show();
+      load();
+    }
+  }
+
+  void dispose() {
+    interstitialAd?.dispose();
+  }
+}
+
+class HomeController {
+  late final UserModel currentUser;
+  int swipedCount = 0;
+  List<String> likedByList = [];
+  final AdsManager adsManager = AdsManager();
+
+  Future<void> initialize(BuildContext context) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    currentUser = userProvider.currentUser!;
+    likedByList = await UserSearchRepo.getLikedByList(currentUser);
+    swipedCount = await UserSearchRepo.getSwipedCount(currentUser);
+    adsManager.load();
+  }
+
+  void incrementSwipe() {
+    swipedCount++;
+    adsManager.showIfNeeded(swipedCount);
+  }
+
+  void dispose() {
+    adsManager.dispose();
+  }
+}
---
+++ lib/features/home/ui/widgets/swipe_card_list.dart
@@ -0,0 +1,151 @@
+import 'dart:developer';
+
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+import 'package:swipable_stack/swipable_stack.dart';
+
+import '../../../../models/user_model.dart';
+import '../../../home/controllers/home_controller.dart';
+import '../../../../common/constants/colors.dart';
+import '../../../../common/providers/theme_provider.dart';
+import '../../bloc/searchuser_bloc.dart';
+import '../../bloc/swipebloc_bloc.dart';
+import '../screens/swipe_card.dart';
+import '../../../match/ui/widget/match_dialog_new.dart';
+
+class SwipeCardList extends StatefulWidget {
+  final HomeController controller;
+  final SwipableStackController? stackController;
+  final Function(UserModel) onUserRemoved;
+
+  const SwipeCardList({
+    super.key,
+    required this.controller,
+    required this.stackController,
+    required this.onUserRemoved,
+  });
+
+  @override
+  State<SwipeCardList> createState() => _SwipeCardListState();
+}
+
+class _SwipeCardListState extends State<SwipeCardList> {
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    return BlocBuilder<SearchUserBloc, SearchUserState>(
+      builder: (context, state) {
+        if (state is SearchUserLoadingState) {
+          widget.stackController?.dispose();
+          return Center(
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation(primaryColor),
+            ),
+          );
+        }
+
+        if (state is SearchUserFailedState) {
+          return Center(
+            child: Text(
+              'Error to load data.'.tr().toString(),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: themeProvider.isDarkMode ? Colors.white : Colors.black54,
+                fontStyle: FontStyle.normal,
+                letterSpacing: 1,
+                decoration: TextDecoration.none,
+                fontSize: 18,
+              ),
+            ),
+          );
+        }
+
+        if (state is SearchUserLoadUserState) {
+          return Container(
+            decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+            height: MediaQuery.of(context).size.height * .78,
+            width: MediaQuery.of(context).size.width,
+            child: state.users.isEmpty
+                ? Align(
+                    alignment: Alignment.center,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.grey[200],
+                            radius: 50,
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Image.asset(
+                                'asset/hookup4u-Logo-BP.png',
+                                fit: BoxFit.contain,
+                                color: primaryColor,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          "There's no one new around you.".tr().toString(),
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: themeProvider.isDarkMode
+                                ? Colors.white
+                                : Colors.black54,
+                            fontStyle: FontStyle.normal,
+                            letterSpacing: 1,
+                            decoration: TextDecoration.none,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                : UsersList(
+                    onswiped: (int index, SwipeDirection dir) {
+                      final user = state.users[index];
+                      if (dir == SwipeDirection.right) {
+                        context.read<SwipeBloc>().add(RightSwipeEvent(
+                              currentUser: widget.controller.currentUser,
+                              selectedUser: user,
+                            ));
+                        if (widget.controller.likedByList.contains(user.id) ||
+                            (user.isBot ?? false)) {
+                          showDialog(
+                            context: context,
+                            builder: (ctx) => MatchDialogPage(
+                              matchedUser: user,
+                              currentUser: widget.controller.currentUser,
+                            ),
+                          );
+                        }
+                        if (index < state.users.length) {
+                          widget.onUserRemoved(user);
+                        }
+                      }
+                      if (dir == SwipeDirection.left) {
+                        context.read<SwipeBloc>().add(LeftSwipeEvent(
+                              currentUser: widget.controller.currentUser,
+                              selectedUser: user,
+                            ));
+                        if (index < state.users.length) {
+                          widget.onUserRemoved(user);
+                        }
+                      }
+                      widget.controller.incrementSwipe();
+                    },
+                    stackController: widget.stackController,
+                    users: state.users,
+                    currentUser: widget.controller.currentUser,
+                    usersList: state.users,
+                  ),
+          );
+        }
+        return const SizedBox.shrink();
+      },
+    );
+  }
+}

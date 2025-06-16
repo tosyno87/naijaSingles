2466b860d1e1e55804010c19d2303e96c74aaf81
Tosin Babatubnde <43211627+tosyno87@users.noreply.github.com>
2025-06-16 08:49:16 -0400

    Implement UserModel toString

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -41,7 +41,6 @@ class UserModel {
     this.age,
     this.address,
     this.isBot,
-    this.isVerified,
     this.latitude,
     this.longitude,
     this.isBlocked,
@@ -64,7 +63,7 @@ class UserModel {
 
   @override
   String toString() {
-
+    return 'UserModel{id: \$id, name: \$name, age: \$age, phone: \$phoneNumber}';
   }
 
   factory UserModel.fromDocument(DocumentSnapshot doc) {
@@ -135,9 +134,6 @@ class UserModel {
           ? List.generate(doc.get('Pictures').length, (index) {
               return doc.get('Pictures')[index];
             })
-          : [],
-      verificationImages: doc.data().toString().contains('verificationImages')
-          ? doc.get('verificationImages')
           : [],
       // distanceBW: doc.get('distanceBW') ?? 0,
     );
